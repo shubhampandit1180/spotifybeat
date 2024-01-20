d390efec2a07a51ce1e2f076c27eea3c8f955c5b
shhubhampandit1180 <shubhampandit4311@gmail.com>
2024-01-20 14:22:31 +0530

    first commit

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function secondsToMinutesAndSeconds(seconds) {
 
 async function getsongs(folder) {
   currfolder = folder;
-  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
 
   let div = document.createElement("div");
